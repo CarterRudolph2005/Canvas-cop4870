3a080a6e84b022d6959cf7c2e64d8ddc198b2ae3
Carter Rudolph <cartergeorge2005@gmail.com>
2026-01-26 16:23:58 -0500

    Update Program.cs
    adding a student or teacher menu. Closes #16

--- CLI.Canvas/Program.cs
+++ CLI.Canvas/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CLI.Canvas;
+// using CLI.CLI.LMS.Model;
+using System;
+
+namespace CLI.Canvas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Main Program Goes Here!
+            Console.WriteLine("Choose one of the following: ");
+            Console.WriteLine("1. Teacher");
+            Console.WriteLine("2. Student");
+            
+            var choice = Console.ReadLine();
+            if(int.TryParse(choice, out int intChoice))
+            {
+                switch (intChoice)
+                {
+                    case 1:
+                        Console.WriteLine("Hello Teacher!");
+
+
+                        break;
+                    case 2:
+                        Console.WriteLine("Hello Student!");
+
+                        break;
+                    default:
+                        Console.WriteLine("ERROR: Invalid input! Try again.");
+                        
+                        break;
+                }
+            }
+            
+        }
+    }
+}
