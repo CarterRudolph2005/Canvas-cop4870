2296a4bb2c7f62059a65a052d08fa341ae854c6b
Carter Rudolph <cartergeorge2005@gmail.com>
2026-03-19 12:35:46 -0400

    Teachers can add assignments. Closes #5.

---
+++ Canvas.Library/Services/AssignmentServiceProxy.cs
@@ -0,0 +1,58 @@
+using System.Runtime.InteropServices;
+using Canvas.Library.Model;
+
+namespace Canvas.Library.Services
+{
+    public class AssignmentServiceProxy
+    {
+        private static AssignmentServiceProxy? instance;
+        private static readonly object instanceLock = new object();
+        public static AssignmentServiceProxy Current
+        {
+            get
+            {
+                lock (instanceLock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new AssignmentServiceProxy();
+                    }
+                }
+                return instance;
+            }
+        }
+        public bool AddAssignment(int CourseID, Assignment assignment)
+        {
+            var course = CourseServiceProxy.Current.Courses.FirstOrDefault(i => i.Id == CourseID);
+            if (course == null) {return false;}
+            if (assignment.Id == 0)
+            {
+                course.Assignments ??= new List<Assignment>();
+                assignment.Id = NextKey(course);
+                course.Assignments.Add(assignment);
+            }
+            return true;
+        }
+
+        private int NextKey(Course course) 
+        {
+            if (course.Assignments != null && course.Assignments.Any()) {
+                return course.Assignments.Max(a => a.Id) + 1;
+            }
+            return 1;
+        }
+
+        // public bool UpdateAssignment(int CourseID, int AssignmentID, Assignment assignmentClone)
+        // {
+        //     var course = CourseServiceProxy.Current.Courses.FirstOrDefault(i => i.Id == CourseID);
+        //     if (course == null) {return false;}
+        //     var assignment = course.Assignments?.FirstOrDefault(i => i.Id == AssignmentID);
+        //     if (assignment == null) {return false;}
+        //     else
+        //     {
+
+        //         return true;   
+        //     }
+        // }
+    }
+}
